8efdbc0a945b7307a2e06a55c6523ba9868e6a49
lac00258 <lac.nt00258@sinhvien.hoasen.edu.vn>
2024-11-27 20:45:40 +0700

    Commit message
    # Conflicts:
    #	lib/app/app_export.dart
    #	lib/app/utils/datetime_utils.dart
    #	lib/app/utils/pref_utils.dart
    #	lib/app/utils/size_utils.dart
    #	lib/main.dart
    #	lib/models/group_model.dart
    #	lib/models/member_model.dart
    #	lib/models/tasks_model.dart
    #	lib/models/user_model.dart
    #	lib/services/auth_service.dart
    #	lib/services/database_service.dart
    #	lib/theme/theme_helper.dart
    #	lib/ui/auth/forgot_password.dart
    #	lib/ui/group_screen/add_group_screen.dart
    #	lib/ui/group_screen/group_screen.dart
    #	lib/ui/home_screen/home_screen.dart
    #	lib/ui/intro_screen/intro_screen.dart
    #	lib/ui/login_screen/login_screen.dart
    #	lib/ui/onboading_screen/onboading1.dart
    #	lib/ui/onboading_screen/onboading2.dart
    #	lib/ui/profile_setting_screen/account_details.dart
    #	lib/ui/profile_setting_screen/profile_setting.dart
    #	lib/ui/register_screen/register_screen.dart
    #	lib/ui/task_screen/add_task_screen.dart
    #	lib/ui/task_screen/edit_task_screen.dart
    #	lib/ui/view_all_task_screen/completed_task_screen.dart
    #	lib/ui/view_all_task_screen/pending_task_screen.dart
    #	lib/ui/view_all_task_screen/today_task_screen.dart
    #	lib/ui/view_all_task_screen/tomorrow_task_screen.dart
    #	lib/widgets/priority_widget.dart
    #	lib/widgets/reusable_text.dart
    #	pubspec.lock
    #	pubspec.yaml

--- lib/ui/group_screen/add_group_screen.dart
+++ lib/ui/group_screen/add_group_screen.dart
@@ -1,17 +1,12 @@
-// ignore_for_file: use_build_context_synchronously, must_be_immutable
 import 'dart:io';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:reminder_app/app/app_export.dart';
 import 'package:reminder_app/widgets/custom_container.dart';
 import 'package:reminder_app/widgets/custom_elevated_button.dart';
 import 'package:reminder_app/widgets/reusable_text.dart';
-import '../../models/group_model.dart';
 
 class AddGroupScreen extends StatefulWidget {
-  Group? group;
-  AddGroupScreen({super.key, this.group});
 
   @override
   State<AddGroupScreen> createState() => _AddGroupScreenState();
@@ -19,77 +14,12 @@ class AddGroupScreen extends StatefulWidget {
 
 class _AddGroupScreenState extends State<AddGroupScreen> {
   final DatabaseService _databaseService = DatabaseService();
-  final ImageService _imageService = ImageService();
   final name = TextEditingController();
   final description = TextEditingController();
-
-  final ImagePicker _picker = ImagePicker();
-
-  File? _imageFile;
-  String? _photoBase64;
 
   bool inSync = false;
   FocusNode nameFocusNode = FocusNode();
   FocusNode descriptionFocusNode = FocusNode();
-
-  @override
-  void initState() {
-    super.initState();
-    _loadGroupPhoto();
-  }
-
-  Future<void> _loadGroupPhoto() async {
-    if (widget.group?.id != null) {
-      var group = await FirebaseFirestore.instance
-          .collection('groups')
-          .doc(widget.group!.id)
-          .get();
-
-      String? photoId = group.data()?['photoId'];
-
-      if (photoId != null) {
-        var imageDoc = await FirebaseFirestore.instance
-            .collection('images')
-            .doc(photoId)
-            .get();
-
-        final base64Image = imageDoc.data()?['image'];
-        if (base64Image != null) {
-          setState(() {
-            _photoBase64 = base64Image;
-            _imageFile = null;
-          });
-        }
-      }
-    }
-  }
-
-  Future<void> _pickImage() async {
-    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
-    if (pickedFile != null) {
-      setState(() {
-        _imageFile = File(pickedFile.path);
-      });
-      _uploadAndSaveImage();
-    }
-  }
-
-  Future<void> _uploadAndSaveImage() async {
-
-    try {
-      String imageId = await _imageService.uploadImage(_imageFile!);
-      await _databaseService.saveImageUrl(widget.group!.id, imageId);
-      var imageDoc = await FirebaseFirestore.instance
-          .collection('images')
-          .doc(imageId)
-          .get();
-      setState(() {
-        _photoBase64 = imageDoc.data()?['image'];
-      });
-    } catch (e) {
-      print('Failed to upload and save image: $e');
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -115,34 +45,7 @@ class _AddGroupScreenState extends State<AddGroupScreen> {
         actions: [
           CustomElevatedButton(
             text: "Save",
-            onPressed: () async {
-              if (name.text.trim().isEmpty) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Group name is required')),
-                );
-                return;
-              }
-
-              try {
-                String? imageId;
-                if (_imageFile != null) {
-                  imageId = await _imageService.uploadImage(_imageFile!);
-                }
-                await _databaseService.createGroup(
-                  name.text,
-                  description.text,
-                  imageId: imageId,
-                );
-
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Group created successfully!')),
-                );
-                Navigator.pop(context);
-              } catch (e) {
-                ScaffoldMessenger.of(context).showSnackBar(
-                  const SnackBar(content: Text('Error saving group')),
-                );
-              }
+              Navigator.pop(context);
             },
             width: 100,
             textStyle: theme.textTheme.bodyLarge,
@@ -164,19 +67,9 @@ class _AddGroupScreenState extends State<AddGroupScreen> {
                         children: [
                           InkWell(
                             onTap: () async {
-                              await _pickImage();
                             },
-                            child: _imageFile != null
-                                ? ClipOval(
-                                    child: Image.file(
-                                      _imageFile!,
-                                      height: 90,
-                                      width: 90,
-                                      fit: BoxFit.cover,
-                                    ),
-                                  )
-                                : const Icon(Icons.image_outlined,
-                                    size: 90, color: Colors.grey),
+                                        height: 90,
+                                        width: 90,
                           ),
                           const SizedBox(width: 10),
                           Expanded(
@@ -214,9 +107,6 @@ class _AddGroupScreenState extends State<AddGroupScreen> {
                       child: Row(
                         children: [
                           IconButton(
-                            onPressed: () {
-                              _showMembersDialog(context);
-                            },
                             icon: const Icon(Icons.person_add_alt_1_outlined,
                                 size: 30, color: Colors.grey),
                           ),
@@ -236,45 +126,6 @@ class _AddGroupScreenState extends State<AddGroupScreen> {
           ],
         ),
       ),
-    );
-  }
-  
-  void _showMembersDialog(BuildContext context) {
-    showDialog(
-      context: context,
-      builder: (context) {
-        return AlertDialog(
-          backgroundColor: appTheme.blackA700,
-          title: const Text(
-            'Add members',
-            style: TextStyle(
-                fontSize: 20, color: Colors.white, fontWeight: FontWeight.w600),
-          ),
-          content: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              TextField(
-                style: const TextStyle(color: Colors.white),
-                decoration: InputDecoration(
-                  hintText: 'Enter email',
-                  hintStyle: TextStyle(color: appTheme.gray400),
-                ),
-              ),
-              const SizedBox(height: 20),
-              CustomElevatedButton(
-                text: "Add",
-                onPressed: () {
-                  // Add member
-
-                },
-                width: 100,
-                textStyle: theme.textTheme.bodyLarge,
-                buttonStyle: CustomButton.none,
-              )
-            ],
-          ),
-        );
-      },
     );
   }
 }
